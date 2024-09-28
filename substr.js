691296fb9a912e109dc8172094a23a4ea3aec5dd
Johncarlo <john.c.flores27@gmail.com>
2024-09-28 13:47:13 -0500

    substring tests for edge cases and examples in substr.js

--- substr.js
+++ substr.js
@@ -2,5 +2,8 @@
 const title = "Guards! Guards!";
 // use console.log(); to see the output
 console.log(title.substring(3)); //> ?
-title.substring(6, 4); //> ?
-title.substring(25); //> ?
+console.log(title.substring(6, 4)); //> ?
+console.log(title.substring(25)); //> ?
+console.log(title.substring(0, 1)); //> ?
+console.log(title.substring(3, 7)); //> ?
+console.log(title.substring(title.length - 6, title.length - 1)); //> ?
