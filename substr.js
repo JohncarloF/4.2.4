9fdda4b874e978fb17c78252fda16a8672cab3f3
Johncarlo <john.c.flores27@gmail.com>
2024-09-28 13:56:22 -0500

    Substring a 8-16 range in substr.js file for clarity.

--- substr.js
+++ substr.js
@@ -1,9 +1,10 @@
 
 const title = "Guards! Guards!";
 // use console.log(); to see the output
-console.log(title.substring(3)); //> ?
-console.log(title.substring(6, 4)); //> ?
-console.log(title.substring(25)); //> ?
-console.log(title.substring(0, 1)); //> ?
-console.log(title.substring(3, 7)); //> ?
-console.log(title.substring(title.length - 6, title.length - 1)); //> ?
+console.log(title.substring(3)); //> prints 3rd index to the end.
+console.log(title.substring(6, 4)); //> Begins at 6 index and 4 is the chacracter count.
+console.log(title.substring(25)); //> 25 is out of range so it prints empty string.
+console.log(title.substring(0)); //> prints from zero 
+console.log(title.substring(0, 1)); //> prints 0th index and 1 chacracter count. 1 is not included.
+console.log(title.substring(0, 3)); //> prints 0th index and 3 chacracter count. 3 is not included.
+console.log(title.substring(8, 16)); //>? 
